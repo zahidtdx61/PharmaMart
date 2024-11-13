514fc93e41abfd632d98c36ecea2e0750346f0b5
Md. Zahidur Rahman <45038726+zahidtdx61@users.noreply.github.com>
2024-11-13 23:49:55 +0600

    added: Banner model with schema for managing banner data

---
+++ server/models/banner.js
@@ -0,0 +1,33 @@
+const { Mongoose } = require("../configs");
+
+const bannerSchema = new Mongoose.Schema({
+  image: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  status: {
+    type: String,
+    required: true,
+    default: "active",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Banner = Mongoose.model("Banner", bannerSchema);
+
+module.exports = Banner;
