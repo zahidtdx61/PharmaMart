ae2a68e27e5abf58e1bf343d4c4668f8e36d2164
Md. Zahidur Rahman <45038726+zahidtdx61@users.noreply.github.com>
2024-11-13 23:47:15 +0600

    added: Category model with schema

---
+++ server/models/category.js
@@ -0,0 +1,24 @@
+const { Mongoose } = require("../configs");
+
+const categorySchema = new Mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Category = Mongoose.model("Category", categorySchema);
+
+module.exports = Category;
