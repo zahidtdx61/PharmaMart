86c2d24e36cfa57e1d9e7d20ca3740bae7ca40c6
Md. Zahidur Rahman <45038726+zahidtdx61@users.noreply.github.com>
2024-11-13 23:43:24 +0600

    added: User Model using mongoose for MongoDB

---
+++ server/models/user.js
@@ -0,0 +1,34 @@
+const { Mongoose } = require("../configs");
+
+const userSchema = new Mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  uid: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  role: {
+    type: String,
+    required: true,
+    default: "user",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  }
+});
+
+const User = Mongoose.model("User", userSchema);
+
+module.exports = User;
