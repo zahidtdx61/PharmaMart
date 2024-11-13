811bf8f54b392eeb124f81b4b985347f991189d8
Md. Zahidur Rahman <45038726+zahidtdx61@users.noreply.github.com>
2024-11-13 15:36:28 +0600

    added: authProvider using firebase

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,16 +3,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { HelmetProvider } from "react-helmet-async";
 import { RouterProvider } from "react-router-dom";
+import AuthProvider from "./AuthProvider/AuthProvider.jsx";
 import "./index.css";
 import router from "./routes/Router.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <HelmetProvider>
-      <CssVarsProvider>
-        <RouterProvider router={router} />
-        <CssBaseline />
-      </CssVarsProvider>
+      <AuthProvider>
+        <CssVarsProvider>
+          <RouterProvider router={router} />
+          <CssBaseline />
+        </CssVarsProvider>
+      </AuthProvider>
     </HelmetProvider>
   </React.StrictMode>
 );
