67e1fe0f44746965fbc3000aa7b066edffd7be01
Md. Zahidur Rahman <45038726+zahidtdx61@users.noreply.github.com>
2024-11-14 20:08:46 +0600

    update:  refactor UserInfo component for improved dropdown menu

--- client/src/components/UserInfo/UserInfo.jsx
+++ client/src/components/UserInfo/UserInfo.jsx
@@ -1,14 +1,13 @@
-import { Avatar, Menu, MenuItem, Typography } from "@mui/joy";
 import { useState } from "react";
 import toast from "react-hot-toast";
 import { Link, useNavigate } from "react-router-dom";
 import useAuth from "../../hooks/useAuth";
+import useAxiosSecure from "../../hooks/useAxiosSecure";
+import { Avatar, Dropdown, Menu, MenuButton, MenuItem } from "@mui/joy";
 
 const UserInfo = () => {
   const { user, logOut, setIsLoading } = useAuth();
   const { photoURL, displayName } = user || {};
-  // const axiosSecure = useAxiosSecure();
-  const [anchorElUser, setAnchorElUser] = useState(null);
   const navigate = useNavigate();
 
   const handleSignOut = async () => {
@@ -26,55 +25,21 @@ const UserInfo = () => {
     }
   };
 
-  const handleOpenUserMenu = (event) => {
-    setAnchorElUser(event.currentTarget);
-  };
-
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
-  };
-
-  const settings = ["Dashboard"];
-
   return (
     <>
       <div className="flex items-center justify-end gap-3">
-        <button onClick={handleOpenUserMenu}>
-          <Avatar src={photoURL} alt={displayName} size="lg" />
-        </button>
+        
 
-        <Menu
-          sx={{ mt: "45px" }}
-          id="menu-appbar"
-          anchorEl={anchorElUser}
-          anchorOrigin={{
-            vertical: "top",
-            horizontal: "right",
-          }}
-          keepMounted
-          transformOrigin={{
-            vertical: "top",
-            horizontal: "right",
-          }}
-          open={Boolean(anchorElUser)}
-          onClose={handleCloseUserMenu}
-        >
-          <MenuItem key={"profile"}>
-            <Typography textAlign="center">{displayName}</Typography>
-          </MenuItem>
+        <Dropdown>
+          <MenuButton className=""><Avatar src={photoURL} /></MenuButton>
+          <Menu>
+            <MenuItem>{displayName}</MenuItem>
+            <MenuItem><Link to="/dashboard">Dashboard</Link></MenuItem>
+            <MenuItem onClick={handleSignOut}>Log out</MenuItem>
+          </Menu>
+        </Dropdown>
 
-          {settings.map((setting) => (
-            <MenuItem key={setting} onClick={handleCloseUserMenu}>
-              <Link to={`/${setting.toLowerCase()}`}>
-                <Typography textAlign="center">{setting}</Typography>
-              </Link>
-            </MenuItem>
-          ))}
-
-          <MenuItem onClick={handleSignOut}>
-            <Typography textAlign="center">Logout</Typography>
-          </MenuItem>
-        </Menu>
+        
       </div>
     </>
   );
