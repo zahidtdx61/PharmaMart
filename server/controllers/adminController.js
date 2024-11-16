a1f9f5bf661226a036c6b48b28098c031310d1b3
Md. Zahidur Rahman <45038726+zahidtdx61@users.noreply.github.com>
2024-11-16 12:05:12 +0600

    added: implement user update functionality in admin controller and routes

--- server/controllers/adminController.js
+++ server/controllers/adminController.js
@@ -1,28 +1,52 @@
 const { StatusCodes } = require("http-status-codes");
 const User = require("../models/user");
 
-const getAllUsers = async(req, res) => {
+const getAllUsers = async (req, res) => {
   try {
     const users = await User.find();
-    return res.status(StatusCodes.OK)
-      .json({
-        success: true,
-        message: "All users",
-        data: users,
-        error: {},
-      })
+    return res.status(StatusCodes.OK).json({
+      success: true,
+      message: "All users",
+      data: users,
+      error: {},
+    });
   } catch (error) {
     // console.log(error);
-    return res.status(StatusCodes.INTERNAL_SERVER_ERROR)
-      .json({
-        success: false,
-        message: "Users not found",
-        data: {},
-        error: error.message,
-      })
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      success: false,
+      message: "Users not found",
+      data: {},
+      error: error.message,
+    });
   }
-}
+};
+
+const updateUser = async (req, res) => {
+  const { id } = req.params;
+  const { body } = req;
+  delete body.uid;
+  body.updatedAt = new Date();
+
+  try {
+    const user = await User.findByIdAndUpdate(id, body);
+    return res.status(StatusCodes.OK).json({
+      success: true,
+      message: "User updated successfully",
+      data: user,
+      error: {},
+    });
+  } catch (error) {
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      success: false,
+      message: "User not found",
+      data: {},
+      error: error.message,
+    });
+  }
+};
+
 
 module.exports = {
   getAllUsers,
-}
+  updateUser,
+};
--- server/routes/v1/admin.js
+++ server/routes/v1/admin.js
@@ -4,5 +4,6 @@ const { AdminController } = require("../../controllers");
 const router = express.Router();
 
 router.get("/all-users", verifyJWT, AdminController.getAllUsers);
+router.put("/update-user/:id", verifyJWT, AdminController.updateUser);
 
 module.exports = router;
